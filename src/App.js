f4f374259e2f94ebec301772aac0fbb82fc0cafb
unknown <ranausamauet3430@gmail.com>
2021-06-07 19:00:32 +0500

    about and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import MyAppbar from "./components/MyAppbar";
-import Home from "./pages/Home";
-import "./css/style.css"
-import "./css/style.min.css"
+import MyAppbar from './components/MyAppbar';
+import Home from './pages/Home';
+import './css/style.css';
+import './css/style.min.css';
+import About from './pages/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div >
+    <div>
       <MyAppbar />
-      <Home />
+      {/* <Home /> */}
+      <About></About>
+      <Footer></Footer>
     </div>
   );
 }
