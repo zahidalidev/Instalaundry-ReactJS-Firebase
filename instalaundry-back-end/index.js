4f7d09557211e2601328e65f244d44a81eaece74
zahidalidev <m.zahidalidev@gmail.com>
2021-06-18 18:48:31 +0500

    new method payment

--- instalaundry-back-end/index.js
+++ instalaundry-back-end/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const apiRouter = require('./routes/routes');
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // using cors to for public API
 app.use(cors())
 app.use(express.json());
 
-app.use('/api/donate', apiRouter);
+app.use('/api/pay', apiRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- instalaundry-back-end/routes/routes.js
+++ instalaundry-back-end/routes/routes.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const stripeLoader = require('stripe');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const stripe = require('stripe')('sk_test_51ISGFTLuBGwlYLhY7bog901ruBLlkTNA0yYaSdFcCIPy16swCvpQbDxovaLfDvYo4eJwseLwOIz40Auk2IqgXZBo00CK4D1bqf');
+
 
 const router = express.Router();
 
-const stripe = new stripeLoader("private key")
-
-const charge = (token, amt) => {
-    return stripe.charges.create({
-        amount: amt * 100,
-        currency: "usd",
-        source: token,
-        description: "statement Description"
-    })
-}
-
 router.post('/', async (req, res, next) => {
-    try {
-        let data = await charge(req.body.token.id, req.body.amount);
-        console.log(data);
-        res.send("charged!")
-    } catch (error) {
-        console.log(error)
-        res.status(500)
-    }
+    const { email } = req.body;
+    console.log(email)
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 500,
+        currency: 'usd',
+        // Verify your integration in this guide by including this parameter
+        metadata: { integration_check: 'accept_a_payment' },
+        receipt_email: email,
+    });
+
+    res.json({ 'client_secret': paymentIntent['client_secret'] })
 });
 
 module.exports = router;
