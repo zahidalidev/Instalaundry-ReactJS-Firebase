f8963e19785662dfb90ba78020bc807f9a99aee3
zahidalidev <m.zahidalidev@gmail.com>
2021-06-25 18:32:51 +0500

    working mailchip

--- instalaundry-fron-end/src/components/common/DefaultFormEmail.js
+++ instalaundry-fron-end/src/components/common/DefaultFormEmail.js
@@ -6,8 +6,12 @@ export default function DefaultFormEmail() {
 
     function sendEmail(e) {
         e.preventDefault();
-
-        emailjs.sendForm('service_siowrj7', 'template_uyzc059', e.target, 'user_ef7lljg2cLfLEVyVsoysv')
+        let body = {
+            'to_name': 'ali',
+            'message': 'engrzahid612@gmail.com',
+            'user_email': 'this is message'
+        }
+        emailjs.sendForm('service_siowrj7', 'template_0bvfsqc', body, 'user_ef7lljg2cLfLEVyVsoysv')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
@@ -17,9 +21,9 @@ export default function DefaultFormEmail() {
 
     return (
         <form className="contact-form" onSubmit={sendEmail}>
-            <input type="hidden" name="contact_number" />
+            <input type="hidden" name="to_name" />
             <label>Name</label>
-            <input type="text" name="user_name" />
+            <input type="text" name="message" />
             <label>Email</label>
             <input type="email" name="user_email" />
             <label>Message</label>
