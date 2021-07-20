0bff448e9bff58b3dacde20531258aeac9d5d214
zahidalidev <m.zahidalidev@gmail.com>
2021-07-20 17:44:42 +0500

    prising loading

--- instalaundry-fron-end/src/components/Slider/PricingPlan.jsx
+++ instalaundry-fron-end/src/components/Slider/PricingPlan.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { Colors } from './../../config/Colors';
 import { getPlans } from '../../services/PricingServices';
 
 export default function PricingPlan() {
   const history = useHistory();
+  const [loading, setLoading] = useState(false);
   const [plans, setPlans] = useState([]);
 
   const getAllPlans = async () => {
+    setLoading(true)
     try {
       const res = await getPlans();
       console.log(res);
@@ -18,6 +21,7 @@ export default function PricingPlan() {
       console.log('Pricing Plans: ', error);
       setPlans([]);
     }
+    setLoading(false)
   };
 
   useEffect(() => {
@@ -46,90 +50,101 @@ export default function PricingPlan() {
         <div className="container">
           <h1 className="display-4 text-center mb-5">Subscription Plans</h1>
           <div className="row">
-            {plans.map((item, index) => (
-              <div key={index} className="col-lg-4 mb-4 ">
-                <div
-                  className="bg-light text-center mb-2 pt-4"
-                  style={{ borderRadius: '1rem', height: '35rem' }}
-                >
-                  <div
-                    className={`d-inline-flex flex-column align-items-center justify-content-center ${
-                      index == 1 ? 'bg-primary' : 'bg-secondary'
-                    } rounded-circle shadow mt-2 mb-4`}
-                    style={{
-                      width: '255px',
-                      height: '250px',
-                      border: '15px solid #ffffff',
-                    }}
-                  >
-                    <h3 className="text-white">{item.name}</h3>
-                    <h1 className="display-4 text-white mb-0">
-                      <small
-                        className="align-top"
-                        style={{ fontSize: '22px', lineHeight: '45px' }}
-                      >
-                        $
-                      </small>
-                      {item.price}
-                      <small
-                        className="align-bottom"
-                        style={{ fontSize: '16px', lineHeight: '40px' }}
-                      >
-                        /Week
-                      </small>
-                    </h1>
-                  </div>
-                  <div className="d-flex flex-column align-items-start py-3">
-                    <p>
-                      <i
-                        className="fa fa-hand-o-right"
-                        aria-hidden="true"
-                        style={{ marginLeft: '1rem' }}
-                      ></i>{' '}
-                      Upto to {item.weight}
-                    </p>
-                    <p>
-                      <i
-                        className="fa fa-hand-o-right"
-                        aria-hidden="true"
-                        style={{ marginLeft: '1rem' }}
-                      ></i>{' '}
-                      {item.size}
-                    </p>
-                    <p>
-                      <i
-                        className="fa fa-hand-o-right"
-                        aria-hidden="true"
-                        style={{ marginLeft: '1rem' }}
-                      ></i>{' '}
-                      Cancel anytime
-                    </p>
-                  </div>
-                  <Button
-                    onClick={() =>
-                      handleSelectPlans({
-                        id: item.id,
-                        planTitle: item.name + ' plan',
-                        planStripeId: item.planStripeId,
-                        price: item.price,
-                      })
-                    }
-                    style={{
-                      backgroundColor:
-                        index === 1 ? Colors.secondary : Colors.primaryBlue,
-                      color: Colors.white,
-                      height: '2.6rem',
-                      width: '9rem',
-                      borderRadius: '0.5rem',
-                    }}
-                    className="btn btn-primary py-md-3 px-md-2 mt-2"
-                    variant="contained"
-                  >
-                    Choose Plan
-                  </Button>
+
+
+            {loading ? (
+              <div className="container align-items-center justify-content-center" style={{ height: "20rem" }} >
+                <div style={{ marginTop: "5rem" }} className="d-flex align-items-center justify-content-center" >
+                  <CircularProgress disableShrink />
                 </div>
               </div>
-            ))}
+            ) : (
+
+              plans.map((item, index) => (
+                <div key={index} className="col-lg-4 mb-4 ">
+                  <div
+                    className="bg-light text-center mb-2 pt-4"
+                    style={{ borderRadius: '1rem', height: '35rem' }}
+                  >
+                    <div
+                      className={`d-inline-flex flex-column align-items-center justify-content-center ${index == 1 ? 'bg-primary' : 'bg-secondary'
+                        } rounded-circle shadow mt-2 mb-4`}
+                      style={{
+                        width: '255px',
+                        height: '250px',
+                        border: '15px solid #ffffff',
+                      }}
+                    >
+                      <h3 className="text-white">{item.name}</h3>
+                      <h1 className="display-4 text-white mb-0">
+                        <small
+                          className="align-top"
+                          style={{ fontSize: '22px', lineHeight: '45px' }}
+                        >
+                          $
+                        </small>
+                        {item.price}
+                        <small
+                          className="align-bottom"
+                          style={{ fontSize: '16px', lineHeight: '40px' }}
+                        >
+                          /Week
+                        </small>
+                      </h1>
+                    </div>
+                    <div className="d-flex flex-column align-items-start py-3">
+                      <p>
+                        <i
+                          className="fa fa-hand-o-right"
+                          aria-hidden="true"
+                          style={{ marginLeft: '1rem' }}
+                        ></i>{' '}
+                        Upto to {item.weight}
+                      </p>
+                      <p>
+                        <i
+                          className="fa fa-hand-o-right"
+                          aria-hidden="true"
+                          style={{ marginLeft: '1rem' }}
+                        ></i>{' '}
+                        {item.size}
+                      </p>
+                      <p>
+                        <i
+                          className="fa fa-hand-o-right"
+                          aria-hidden="true"
+                          style={{ marginLeft: '1rem' }}
+                        ></i>{' '}
+                        Cancel anytime
+                      </p>
+                    </div>
+                    <Button
+                      onClick={() =>
+                        handleSelectPlans({
+                          id: item.id,
+                          planTitle: item.name + ' plan',
+                          planStripeId: item.planStripeId,
+                          price: item.price,
+                        })
+                      }
+                      style={{
+                        backgroundColor:
+                          index === 1 ? Colors.secondary : Colors.primaryBlue,
+                        color: Colors.white,
+                        height: '2.6rem',
+                        width: '9rem',
+                        borderRadius: '0.5rem',
+                      }}
+                      className="btn btn-primary py-md-3 px-md-2 mt-2"
+                      variant="contained"
+                    >
+                      Choose Plan
+                    </Button>
+                  </div>
+                </div>
+              ))
+            )}
+
           </div>
         </div>
       </div>
