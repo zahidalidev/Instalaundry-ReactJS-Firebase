2869eae1b72cbe3486c17356c382a2570ce23b98
RANA-USAMA <ranausamauet3430@gmail.com>
2021-09-30 01:05:10 +0500

    Profile page reformated,avatr set,navigations

--- instalaundry-fron-end/src/config/db.js
+++ instalaundry-fron-end/src/config/db.js
@@ -1,12 +1,5 @@
-import Firebase from 'firebase';
-import 'firebase/firestore';
+
 
 export const firebaseConfig = {
-    apiKey: "Firebase Key here!",
-    authDomain: "instalaundary-77ed4.firebaseapp.com",
-    projectId: "instalaundary-77ed4",
-    storageBucket: "instalaundary-77ed4.appspot.com",
-    messagingSenderId: "615509243840",
-    appId: "1:615509243840:web:a1c2750131ff7bec2bfa25",
-    measurementId: "G-73JK93YE1X"
+
 }
